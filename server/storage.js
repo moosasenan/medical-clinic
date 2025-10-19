c2058cbee319a862603d09cb4eb0c3668f438acb
moosasenan <moosa.senan@gmail.com>
2025-10-19 08:06:29 +0300

    Update storage.js

--- server/storage.js
+++ server/storage.js
@@ -1,68 +1,28 @@
 import { db } from './db.js';
 
 export class DatabaseStorage {
-    // المستخدمين
     getUserByUsername(username) {
         return db.getUserByUsername(username);
     }
-
-    getUserById(id) {
-        return db.getUserById(id);
-    }
-
-    async createUser(username, passwordHash, role = 'user') {
+    
+    async createUser(username, passwordHash, role) {
         return await db.createUser(username, passwordHash, role);
     }
-
-    // المرضى
+    
     getAllPatients() {
         return db.getAllPatients();
     }
-
-    getPatientById(id) {
-        return db.getPatientById(id);
-    }
-
+    
     async createPatient(patientData) {
         return await db.createPatient(patientData);
     }
-
-    async updatePatient(id, updates) {
-        return await db.updatePatient(id, updates);
-    }
-
-    async deletePatient(id) {
-        return await db.deletePatient(id);
-    }
-
-    // المواعيد
+    
     getAllAppointments() {
         return db.getAllAppointments();
     }
-
-    getAppointmentById(id) {
-        return db.getAppointmentById(id);
-    }
-
+    
     async createAppointment(appointmentData) {
         return await db.createAppointment(appointmentData);
-    }
-
-    async updateAppointment(id, updates) {
-        return await db.updateAppointment(id, updates);
-    }
-
-    async deleteAppointment(id) {
-        return await db.deleteAppointment(id);
-    }
-
-    // الإحصائيات
-    getStats() {
-        return {
-            patients: db.data.patients.length,
-            appointments: db.data.appointments.length,
-            users: db.data.users.length
-        };
     }
 }
 
